388a75651ebc6f117e8ad14599dd443ee8b3fcb6
Thiago Egon Lange <thiago_lange@yahoo.com.br>
2022-11-02 16:11:54 -0300

    First Load

---
+++ dbt_airbyte/models/dw_faturamento_ano.sql
@@ -0,0 +1,12 @@
+
+with dw_faturamento_ano as (
+
+    select DATE_PART('year', purchased_at) AS "Ano"
+        ,sum(ap.price) as "Faturamento"
+    from public.airbyte_purchases pur inner join airbyte_products ap
+    on pur.product_id = ap.id
+    group by 1
+    order by 1 DESC
+)
+
+select * from dw_faturamento_ano
---
+++ dbt_airbyte/models/dw_faturamento_model.sql
@@ -0,0 +1,12 @@
+
+with dw_faturamento_model as (
+
+    select pro.model as "Model"
+        ,sum(pro.price) as "Faturamento"
+    from public.airbyte_purchases pur inner join airbyte_products pro
+    on pur.product_id = pro.id
+    group by pro.model
+    order by 2 DESC
+)
+
+select * from dw_faturamento_model
